d4c062e3e40ceb57780fa85f7cde8cfee6b34a98
Amanda Walker <walker.akw@gmail.com>
2020-07-09 06:36:29 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,7 +1,9 @@
-require 'pry'
-
-array = ["Tim Jones", "Tom Smith", "Jim Campagno"]
-my_collect(array) do |name|
-  name.split(" ").first
-  binding.pry
+def my_collect(array)
+  i = 0
+  collection = []
+  while i < array.length
+    collection << yield(array[i])
+    i +=1
+  end
+  collection
 end
